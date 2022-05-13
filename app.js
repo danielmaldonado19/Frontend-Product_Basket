704c08b4096042b4335cfeb5ee803bfd28b778d0
Daniel Maldonado <damaldy19@hotmail.com>
2022-05-12 21:18:36 -0500

    Starting point for app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+- product-form can be described as an object, this is 
+because the form itself has name, price, description
+atributes. This form has methods as save product, list
+product, etc.
+- The interface can be considered as an object as well.
+Thas because it can activate a method by click some btn
+and call a product method or something else.
+*/
